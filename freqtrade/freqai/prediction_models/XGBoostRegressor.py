ecc70fa96284eec8b29dd8b7324c89357a11f5ce
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-02-07 11:34:50 +0100

    refactor(freqai): remove also the train sets from eval_set
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- freqtrade/freqai/prediction_models/XGBoostRegressor.py
+++ freqtrade/freqai/prediction_models/XGBoostRegressor.py
@@ -36,7 +36,7 @@ class XGBoostRegressor(BaseRegressionModel):
             eval_set = None
             eval_weights = None
         else:
-            eval_set = [(data_dictionary["test_features"], data_dictionary["test_labels"]), (X, y)]
+            eval_set = [(data_dictionary["test_features"], data_dictionary["test_labels"])]
             eval_weights = [data_dictionary["test_weights"]]
 
         sample_weight = data_dictionary["train_weights"]
